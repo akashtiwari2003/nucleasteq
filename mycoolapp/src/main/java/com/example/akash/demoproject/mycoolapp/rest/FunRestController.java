f00106858b3a10dea056abded894b41ad3503fed
Akash Tiwari <akashtiwari722003@gmail.com>
2024-03-31 04:55:32 +0530

    new-endpoint

--- mycoolapp/src/main/java/com/example/akash/demoproject/mycoolapp/rest/FunRestController.java
+++ mycoolapp/src/main/java/com/example/akash/demoproject/mycoolapp/rest/FunRestController.java
@@ -9,4 +9,9 @@ public class FunRestController {
     public String sayHello(){
         return ("Hello World!!");
     }
+
+    @GetMapping("/workout")
+    public String getDailyWorkout(){
+        return ("Run a hard 5k");
+    }
 }
