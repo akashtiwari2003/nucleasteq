cc1e0035b579949c426779d78f16b900f1154467
Akash Tiwari <akashtiwari722003@gmail.com>
2024-04-03 03:54:45 +0530

    custom_values

--- mycoolapp/src/main/java/com/example/akash/demoproject/mycoolapp/rest/FunRestController.java
+++ mycoolapp/src/main/java/com/example/akash/demoproject/mycoolapp/rest/FunRestController.java
@@ -1,10 +1,17 @@
 package com.example.akash.demoproject.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
     @GetMapping("/")
     public String sayHello(){
         return ("Hello World!!");
@@ -13,5 +20,10 @@ public class FunRestController {
     @GetMapping("/workout")
     public String getDailyWorkout(){
         return ("Run a hard 5k");
+    }
+
+    @GetMapping("/customvalues")
+    public String getCustomValues(){
+        return (coachName + " from " + teamName);
     }
 }
